85a79bc8e3af25c6a84d63dc67af3dc59b77d9b7
nomius <dcortarello@gmail.com>
2011-08-04 18:43:11 +0000

    Added support for shell scripting and some extra options too (-h for ms macro head and -l to choose, which can be C for C and S for shell scripting)

--- C2troff.c
+++ C2troff.c
@@ -46,7 +46,8 @@
 	return -1; \
 }
 
-#define MAX_LINE 4096
+/* Pretty long lines!!! */
+#define MAX_LINE 65535
 #define TABS_SIZE 4
 
 const char *reserved_C[] = {
@@ -56,12 +57,18 @@ const char *reserved_C[] = {
 	"static", "struct", "switch", "typedef", "union", "unsigned", "void", 
 	"volatile", "while", "#define", "#else", "#endif", "#if", "#ifdef", 
 	"#ifndef", "#include", "#undef", "#", "define", "endif", "ifdef", "ifndef", 
-	"include", "undef"
+	"include", "undef", NULL
 };
 
-const char *delimiters_C = " \t(){}.[]*:;=\\\n";
+const char *reserved_SH[] = {
+	"if", "fi", "then", "exit", "echo", "-ne", "-eq", "-gt", "-lt", "-ge", "-le" 
+	"read", "do", "done", "for", "case", "esac", "break", "continue", "until", 
+	"time", "select", "in", "function", "else", "elif", "[[", "]]", NULL
+};
+
+const char *delimiters_C =  " \t(){}.[]*&:;+-=\\\n";
+const char *delimiters_SH = " \t(){}.[]*&:;+-=*|<>`\\\n";
 const int max_reserved = 8;
-const int size_reserved = 46;
 
 typedef struct _mtok {
 	char orig_token[10];
@@ -70,13 +77,13 @@ typedef struct _mtok {
 } mtok;
 
 
-int rtoken(char *str, mtok *dat)
+int rtoken(char *str, const char *reserved[], const char *delimiters, mtok *dat)
 {
 	int i = 0, j = 0;
 
 	for (i = 0; str[i] && i < max_reserved; i++) {
-		for (j = 0; delimiters_C[j]; j++)
-			if (str[i] == delimiters_C[j])
+		for (j = 0; delimiters[j]; j++)
+			if (str[i] == delimiters[j])
 				goto gcopy;
 	}
 
@@ -97,8 +104,9 @@ gcopy:
 			strcpy(dat->out_token, "\\fB}\\fP");
 	}
 
-	for (i = 0; i < size_reserved; i++)
-		if (!memcmp(dat->orig_token, reserved_C[i], strlen(reserved_C[i]))) {
+	for (i = 0; reserved[i]; i++)
+		if (strlen(reserved[i]) == strlen(dat->orig_token) && 
+			 !memcmp(dat->orig_token, reserved[i], strlen(dat->orig_token))) {
 			sprintf(dat->out_token, "\\fB%s\\fP", dat->orig_token);
 			break;
 		}
@@ -190,7 +198,7 @@ void doC(FILE *input, FILE *output)
 
 			/* Otherwise, print the character taking care for reserved words */
 			else {
-				j = rtoken(line+i, &dat);
+				j = rtoken(line+i, reserved_C, delimiters_C, &dat);
 				fprintf(output, "%s", dat.out_token);
 				i += j-1;
 			}
@@ -198,40 +206,137 @@ void doC(FILE *input, FILE *output)
 	}
 }
 
-void doShell(FILE *input, FILE *output)
+
+void doSH(FILE *input, FILE *output)
 {
+	char line[MAX_LINE];
+	int comment = 0, i = 0, tabs = 0, j = 0;
+	mtok dat;
+
+	while (fgets(line, MAX_LINE, input) != NULL) {
+
+		/* Check for an empty line */
+		if (*line == '\0' || *line == '\n') {
+			fprintf(output, "\n.br\n");
+			continue;
+		}
+
+		for (i = 0; i < strlen(line); i++) {
+
+			/* Comment, so print the rest of the line and jump to the next one */
+			if (line[i] == '#' ) {
+				fprintf(output, line+i);
+            fprintf(output, ".br\n");
+				break;
+			}
+
+			/* Check for tab */
+			if (line[i] == '\t') {
+				/* Count how many tabs we have */
+				for (tabs = 1; line[i] && line[i+tabs] == '\t'; tabs++)
+					;
+				i += tabs - 1;
+				fprintf(output, "\\h'|%dn'", TABS_SIZE * tabs);
+			}
+
+			/* Check for a pointer * */
+			else if (line[i] == '*')
+				fprintf(output, "\\fI*\\fP");
+
+			/* Check for an inverted slash (\) */
+			else if (line[i] == '\\')
+				fprintf(output, "\\e");
+
+			/* Check for an inverted slash (\) */
+			else if (line[i] == '.')
+				fprintf(output, "\\&.");
+
+			/* Check for a opening braket ({) */
+			else if (line[i] == '{')
+				fprintf(output, "\\fB{\\fP");
+
+			/* Check for a clossing braket (}) */
+			else if (line[i] == '}')
+				fprintf(output, "\\fB}\\fP");
+
+			/* Check for a clossing braket (}) */
+			else if (line[i] == '`')
+				fprintf(output, "\\(oq");
+
+			/* Check for a carriage return (\n) */
+			else if (line[i] == '\n')
+				fprintf(output, "\n.br\n");
+
+			/* Otherwise, print the character taking care for reserved words */
+			else {
+				j = rtoken(line+i, reserved_SH, delimiters_SH, &dat);
+				fprintf(output, "%s", dat.out_token);
+				i += j-1;
+			}
+		}
+	}
 }
 
+void help_exit() {
+	fprintf(stderr, "Usage: C2troff <OPTIONS> [<INPUT FILE>] [<OUTPUT FILE>]\n"
+						 "OPTIONS:\n"
+						 "-l <LANGUAGE> - Supported languages: C and S (shell)\n"
+						 "-h <y/n>      - Include ms macro head in the output\n"
+						 "-m <y/n>      - Use monospace font\n\n");
+	exit(1);
+}
 
 int main(int argc, char *argv[])
 {
 	FILE *input = NULL, *output = NULL;
-	char language = 'C';
+	char language = 'C', head = 'y';
+	short int i = 0;
+
+	if (argc < 5)
+		help_exit();
+
+	for (i = 1; i < 5; i++) {
+		if (!strcmp(argv[i], "-l")) {
+			language = argv[i+1][0];
+			i += 2;
+		}
+		if (!strcmp(argv[i], "-h")) {
+			head = argv[i+1][0];
+			i += 2;
+		}
+	}
 
 	switch (argc) {
-		case 1:
+		case 5:
 			output = stdout;
 			input = stdin;
 			break;
-		case 2:
-			if (!(input = fopen(argv[1], "r")))
+		case 6:
+			if (!(input = fopen(argv[5], "r")))
 				pferror("fopen");
 			output = stdout;
 			break;
-		case 3:
-			if (!(input = fopen(argv[1], "r")))
+		case 7:
+			if (!(input = fopen(argv[5], "r")))
 				pferror("fopen");
-			if (!(output = fopen(argv[2], "w")))
+			if (!(output = fopen(argv[6], "w")))
 				pferror("fopen");
 			break;
 	}
-	
+
+	if (head == 'y') {
+		fprintf(output, ".nr PS 12\n");
+		fprintf(output, ".nr PO 1i\n");
+		fprintf(output, ".nr LL 6.5i\n");
+		fprintf(output, ".nr LH 2i\n");
+	}
+
 	switch (language) {
 		case 'C':
 			doC(input, output);
 			break;
 		case 'S':
-			doShell(input, output);
+			doSH(input, output);
 			break;
 	}
 
